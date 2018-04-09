9d0f00d55b78312d7ca6021ee81b1875625ce8f8
Donaldo Lopez <LUIS12_94@hotmail.com>
2018-04-08 23:37:22 -0500

    Avance maps con package

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -22,6 +22,85 @@
         });
       }, 500);
     });
+    var map;
+    var marker;
+
+    function placeMarker(location) {
+      if (marker) {
+        marker.setPosition(location);
+      } else {
+        marker = new google.maps.Marker({
+          position: location,
+          map: map,
+          draggable: true
+        });
+      }
+    }
+
+    function initialize() {
+      var centerPosition = new google.maps.LatLng(19.3910038, -99.2836973);
+      var options = {
+        zoom: 6,
+        center: centerPosition,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      };
+      map = new google.maps.Map($('#map')[0], options);
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+
+          map.setCenter(pos);
+          placeMarker(pos);
+          map.setZoom(14);
+
+        }, function() {
+          handleLocationError(true, infoWindow, map.getCenter());
+        });
+      } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+      }
+
+      var input = document.getElementById('searchTextField');
+      var autocomplete = new google.maps.places.Autocomplete(input, {
+        types: ["geocode"]
+      });
+      var geocoder = new google.maps.Geocoder;
+
+      autocomplete.bindTo('bounds', map);
+      var infowindow = new google.maps.InfoWindow();
+
+      google.maps.event.addListener(autocomplete, 'place_changed', function(event) {
+        infowindow.close();
+        var place = autocomplete.getPlace();
+        if (place.geometry.viewport) {
+          map.fitBounds(place.geometry.viewport);
+        } else {
+          map.setCenter(place.geometry.location);
+          map.setZoom(17);
+        }
+
+      });
+      google.maps.event.addListener(map, 'click', function(evt) {
+        placeMarker(evt.latLng);
+        $('#latitud').val(evt.latLng.lat().toFixed(3));
+        $('#longitud').val(evt.latLng.lng().toFixed(3));
+        //geocodeLatLng(geocoder, map, infowindow);
+      });
+      @isset($sucursal)
+      var pos = {
+        lat: {!!$sucursal - > latitud!!
+        },
+        lng: {!!$sucursal - > longitud!!
+        }
+      };
+      map.setCenter(pos);
+      @endisset
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
   </script>
 </body>
 
