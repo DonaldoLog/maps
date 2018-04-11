45cad2e49ddae8430dbad20baf967a52b8600b6d
Donaldo Lopez <LUIS12_94@hotmail.com>
2018-04-10 23:07:32 -0500

    con paquete

--- app/Http/Controllers/MapsController.php
+++ app/Http/Controllers/MapsController.php
@@ -12,11 +12,68 @@ public function index()
 {
   //  Mapper::map(53.381128999999990000, -1.470085000000040000,['draggable' => true,'eventDragEnd' => 'console.log(event.latLng.lat()); console.log(event.latLng.lng());','zoom' => 4, 'marker' => false, 'eventBeforeLoad' => 'addEventListener(map);']);
 //Mapper::map(40, -100, ['zoom' => 4, 'marker' => false, 'type' => MapperBase::TYPE_ROADMAP, 'eventBeforeLoad' => 'addEventListener(map);']);
-  Mapper::map(40, -100, ['zoom' => 4, 'marker' => false, 'eventBeforeLoad' => "google.maps.event.addListener(map, 'click', function(evt) {
+  Mapper::map(19.3910038,-99.2836973, ['zoom' => 4, 'marker' => false, 'eventBeforeLoad' => "
+  var marker='';
+  function placeMarker(location) {
+  if (marker) {
+  console.log('marjer');
+  marker.setPosition(location);
+  } else {
+  console.log('marjer2');
+
+  marker = new google.maps.Marker({
+  position: location,
+  map: map,
+  draggable: true
+  });
+  }
+  }
+        google.maps.event.addListener(map, 'click', function(evt) {
     dire=evt.latLng;
     console.log('this:'+dire)
-    placeMarker(dire,map);
-  });"]);
+    placeMarker(dire);
+  });
+  var input = document.getElementById('searchTextField');
+  var autocomplete = new google.maps.places.Autocomplete(input, {
+    types: ['geocode']
+  });
+  var geocoder = new google.maps.Geocoder;
+
+  autocomplete.bindTo('bounds', map);
+  var infowindow = new google.maps.InfoWindow();
+
+  google.maps.event.addListener(autocomplete, 'place_changed', function(event) {
+    infowindow.close();
+    var place = autocomplete.getPlace();
+    if (place.geometry.viewport) {
+      map.fitBounds(place.geometry.viewport);
+    } else {
+      map.setCenter(place.geometry.location);
+      map.setZoom(17);
+    }
+});
+
+if (navigator.geolocation) {
+  console.log('g');
+   navigator.geolocation.getCurrentPosition(function(position) {
+     var pos = {
+       lat: position.coords.latitude,
+       lng: position.coords.longitude
+     };
+
+     map.setCenter(pos);
+     console.log(pos);
+     placeMarker(pos);
+     map.setZoom(14);
+
+   }, function() {
+     handleLocationError(true, infoWindow, map.getCenter());
+   });
+ } else {
+   // Browser doesn't support Geolocation
+   handleLocationError(false, infoWindow, map.getCenter());
+ }
+  "]);
     return view('map');
 }
 }
--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -7,6 +7,9 @@
 </head>
 
 <body>
+
+<input id="searchTextField" class='form-control' placeholder="Buscar en Google Maps">
+<div id="results"></div>
   <div style="width: 500px; height: 500px;">
     {!! Mapper::render() !!}
   </div>
@@ -15,27 +18,24 @@
   			  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
   			  crossorigin="anonymous"></script>
           <script type="text/javascript">
-              var marker="";
-          function placeMarker(location,map) {
-            if (marker) {
-              console.log('marjer');
-            marker.setPosition(location);
-            } else {
-              console.log('marjer2');
-
-            marker = new google.maps.Marker({
-            position: location,
-            map: map,
-            draggable: true
-            });
-            }
-            }
+          //     var marker="";
+          //
+          // function placeMarker(location,map) {
+          //   if (marker) {
+          //     console.log('marjer');
+          //   marker.setPosition(location);
+          //   } else {
+          //     console.log('marjer2');
+          //
+          //   marker = new google.maps.Marker({
+          //   position: location,
+          //   map: map,
+          //   draggable: true
+          //   });
+          //   }
+          //   }
 
           </script>
-
-
-
-
 
 
 
