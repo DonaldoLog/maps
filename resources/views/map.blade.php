de649dd4e2dc47d477232d43a9ff39d82dab9435
Donaldo Lopez <LUIS12_94@hotmail.com>
2018-04-10 23:21:35 -0500

    Javascript implementado

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -8,36 +8,57 @@
 
 <body>
 
-<input id="searchTextField" class='form-control' placeholder="Buscar en Google Maps">
-<div id="results"></div>
+  <input id="searchTextField" class='form-control' placeholder="Buscar en Google Maps">
+  <div id="results"></div>
   <div style="width: 500px; height: 500px;">
     {!! Mapper::render() !!}
   </div>
-  <script
-  			  src="https://code.jquery.com/jquery-3.3.1.min.js"
-  			  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
-  			  crossorigin="anonymous"></script>
-          <script type="text/javascript">
-          //     var marker="";
-          //
-          // function placeMarker(location,map) {
-          //   if (marker) {
-          //     console.log('marjer');
-          //   marker.setPosition(location);
-          //   } else {
-          //     console.log('marjer2');
-          //
-          //   marker = new google.maps.Marker({
-          //   position: location,
-          //   map: map,
-          //   draggable: true
-          //   });
-          //   }
-          //   }
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  <script type="text/javascript">
+    function addMarkerListener(map) {
+      var marker;
+      function placeMarker(location) {
+        if (marker) {
+          marker.setPosition(location);
+        } else {
+          marker = new google.maps.Marker({
+            position: location,
+            map: map,
+            draggable: true
+          });
+        }
+      }
+      var input = document.getElementById('searchTextField');
+      var autocomplete = new google.maps.places.Autocomplete(input);
 
-          </script>
+      autocomplete.bindTo('bounds', map);
+      var infowindow = new google.maps.InfoWindow();
+            var infowindowContent = document.getElementById('infowindow-content');
+            infowindow.setContent(infowindowContent);
+            var marker = new google.maps.Marker({
+              map: map,
+              anchorPoint: new google.maps.Point(0, -29)
+            });
 
+      autocomplete.addListener('place_changed', function(event) {
+          console.log('cambiado');
+        infowindow.close();
+        var place = autocomplete.getPlace();
+        console.log(place);
+        if (place.geometry.viewport) {
+          map.fitBounds(place.geometry.viewport);
+        } else {
+          map.setCenter(place.geometry.location);
+          map.setZoom(17);
+        }
 
+      });
+      map.addListener('click', function(evt) {
+        placeMarker(evt.latLng);
+      });
+
+    }
+  </script>
 
 </body>
 
